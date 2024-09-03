781bfcdf22c6baffefac3d8a9260a1fc0fdf13d1
Manish Gohel <gohel2567@gmail.com>
2024-09-04 01:09:13 +0530

    Update wsgi.py

--- sintron/sintron/wsgi.py
+++ sintron/sintron/wsgi.py
@@ -16,7 +16,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sintron.settings')
 application = get_wsgi_application()
 
 app = application
-
-
-if os.environ.get('ENV') == 'production':
-    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
